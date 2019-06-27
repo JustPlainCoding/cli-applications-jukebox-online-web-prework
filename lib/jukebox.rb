813ecd7506fb05c3d77c4b3e033de2edecc613dd
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-27 18:06:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,7 +1,7 @@
 def help
   puts "I accept the following commands:"
-- help : displays this help message
-- list : displays a list of songs you can play
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
 - play : lets you choose a song to play
 - exit : exits this program
 end
